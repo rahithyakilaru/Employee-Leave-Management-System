092296b9241298b1cd43188556d059e60cc7ff82
Rahithya Kilaru <rahithyakilaru9@gmail.com>
2025-05-12 20:37:01 -0500

    Delete Employee DB directory

--- Employee DB/Department.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Employee_DB
-{
-    public class Department
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Employee> Employees { get; set; }
-    }
-}
